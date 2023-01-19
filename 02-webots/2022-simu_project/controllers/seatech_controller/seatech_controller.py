a83573b27bc36411fbc7d5cb99ba727dc1b4141d
tatayoyoh <tatayoyoh@mymail>
2023-01-19 19:35:24 +0100

    Rennomage du projet simu 2022

--- 02-webots/2022-simu_project/controllers/seatech_controller/seatech_controller.py
+++ 02-webots/2022-simu_project/controllers/seatech_controller/seatech_controller.py
@@ -1,10 +1,14 @@
 from controller import Robot, Motor, Camera, DistanceSensor, Keyboard
 
+KEYBOARD_SAMPLING_PERIOD = 200
+TIME_STEP = 64
+
 class RobotMotor(Motor):
     def __init__(self, name):
         super().__init__(name)
         self.setPosition(float('inf'))
         self.setVelocity(0)
+        self.max_speed = self.getMaxVelocity()
 
 class SeatechRobot(Robot):
     def __init__(self):
@@ -13,17 +17,15 @@ class SeatechRobot(Robot):
         self.__rightMotor = RobotMotor('right wheel motor')
 
     def run(self):
-        self.__leftMotor.setVelocity(MAX_SPEED)
-        self.__rightMotor.setVelocity(MAX_SPEED)
+        self.__leftMotor.setVelocity(self.max_speed)
+        self.__rightMotor.setVelocity(self.max_speed)
 
 if __name__ == '__main__':
-    TIME_STEP = 64
-    MAX_SPEED = 6.28
     # create the Robot instance.
     robot = SeatechRobot()
     keyboard =  Keyboard()
-    keyboard.enable(samplingPeriod=100)
-    robot.run()
+    keyboard.enable(KEYBOARD_SAMPLING_PERIOD)
     while robot.step(TIME_STEP) != -1:
         if keyboard.getKey() == keyboard.UP:
             print('salut')
+            robot.run()
